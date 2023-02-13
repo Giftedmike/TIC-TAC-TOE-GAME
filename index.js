ff2fea57c77ac797797507506f724d760092825e
Michael Patrick <michael.patrick.org@gmail.com>
2023-02-13 12:48:33 -0800

    added players turn text

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var player = 1;
 
 function playerAct(elem){
+if(elem.innerHTML != "") return;
+
     if(player == 1){
         elem.innerHTML = 'p1'
+        document.getElementById('message').innerHTML = "Player 2 Your Turn";
         player = 2;
     }
 
     else if(player == 2){
         elem.innerHTML = 'p2'
+        document.getElementById('message').innerHTML = "Player 1 Your Turn";
         player = 1;
     }
 }
