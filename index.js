05c94aee41a690744f3bd68642f318b2ddfe97e4
Michael Patrick <michael.patrick.org@gmail.com>
2023-02-20 06:13:12 -0800

    Trying to write a code that determines a winner using conditional statements

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var player = 1;
+var col1; 
+var col2; 
+var col3; 
+var col4; 
+var col5; 
+var col6; 
+var col7; 
+var col8; 
+var col9;
 
-function playerAct(elem){
-if(elem.innerHTML != "") return;
 
+function playerAct(elem, row, col){
+    if(elem.innerHTML != "") return;
+
+        
     if(player == 1){
         elem.innerHTML = 'p1'
         document.getElementById('message').innerHTML = "Player 2 Your Turn";
@@ -14,4 +25,17 @@ if(elem.innerHTML != "") return;
         document.getElementById('message').innerHTML = "Player 1 Your Turn";
         player = 1;
     }
+
+//Horizontal winning
+    if((col1 == 'p1') && (col2 == 'p1') && (col3 == 'p1')){
+        document.getElementById('col4').disabled = true;
+        document.getElementById('col5').disabled = true;
+        document.getElementById('col6').disabled = true;
+        document.getElementById('col7').disabled = true;
+        document.getElementById('col8').disabled = true;
+        document.getElementById('col9').disabled = true;
+        alert('player 1 wins')
+    }
 }
+
+
